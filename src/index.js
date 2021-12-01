5ddb147c8840b4d5443985de0c64c45e861770ff
Lou <93538754+louboo@users.noreply.github.com>
2021-12-01 22:43:55 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,65 +1,101 @@
-let weatherType = ["Sun", "Rain", "Cloud", "Snow"];
+function citySearch(event) {
+  event.preventDefault()
+}
 
-new Date();
-let now = new Date();
+function displayCurrentTemp(response) {
+  let temp = response.data.main.temp
+  document.querySelection()
+}
 
-let date = now.getDate();
-let hours = now.getHours();
-let minutes = (now.getMinutes() < 10 ? "0" : "") + now.getMinutes();
-let seconds = now.getSeconds();
-let year = now.getFullYear();
+let weatherType = ['Sun', 'Rain', 'Cloud', 'Snow']
+
+new Date()
+let now = new Date()
+
+let date = now.getDate()
+let hours = now.getHours()
+let minutes = (now.getMinutes() < 10 ? '0' : '') + now.getMinutes()
+let seconds = now.getSeconds()
+let year = now.getFullYear()
 
 let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+]
+let day = days[now.getDay()]
 
 let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "Augusut",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-let month = months[now.getMonth()];
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'Augusut',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+let month = months[now.getMonth()]
 
-let h3 = document.querySelector("h3");
-h3.innerHTML = `${day}, ${date} ${month}, ${year}`;
-let h4 = document.querySelector("h4");
-h4.innerHTML = `${hours}:${minutes}`;
+let h3 = document.querySelector('h3')
+h3.innerHTML = `${day}, ${date} ${month}, ${year}`
+let h4 = document.querySelector('h4')
+h4.innerHTML = `${hours}:${minutes}`
 
-function displayCity(event) {
-  event.preventDefault();
-  let input = document.querySelector("#search-text-input");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `${input.value}`;
+// function convertToC() {
+// let fTemp = 68
+// let cTemp = Math.round(((fTemp - 32) * 5) / 9)
+// let h2 = document.querySelector('h2')
+// h2.innerHTML = `${cTemp}°C`
+// }
+
+// WEATHER API
+
+function showTemp(response) {
+  let cityName = response.data.name
+  let h1 = document.querySelector('h1')
+  h1.innerHTML = cityName
+  let temp = document.querySelector('h2')
+  temp.innerHTML = `${Math.round(response.data.main.temp)}°C`
+  let weatherDesc = document.querySelector('h6')
+  weatherDesc.innerHTML = `The weather today is...${response.data.weather[0].description}`
 }
-let form = document.querySelector("#search-bar");
-form.addEventListener("submit", displayCity);
 
-function convertToF() {
-  let cTemp = 20;
-  let fTemp = Math.round((cTemp * 9) / 5 + 32);
-  let h2 = document.querySelector("h2");
-  h2.innerHTML = `${fTemp}°F`;
+function citySearch(city) {
+  let apiKey = '270ea88a43eb3b55ea7828fd11903447'
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemp)
 }
 
-function convertToC() {
-  let fTemp = 68;
-  let cTemp = Math.round(((fTemp - 32) * 5) / 9);
-  let h2 = document.querySelector("h2");
-  h2.innerHTML = `${cTemp}°C`;
+function buttonSubmit(event) {
+  event.preventDefault()
+  let city = document.querySelector('#search-text-input').value
+  citySearch(city)
 }
+
+let form = document.querySelector('#search-bar')
+form.addEventListener('submit', buttonSubmit)
+
+function showPosition(position) {
+  let apiKey = '270ea88a43eb3b55ea7828fd11903447'
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`
+  axios.get(apiUrl).then(showTemp)
+}
+
+function currentTemps(event) {
+  event.preventDefault()
+  navigator.geolocation.getCurrentPosition(showPosition)
+}
+
+let button = document.querySelector('#currentLoc')
+button.addEventListener('click', currentTemps)
+
+navigator.geolocation.getCurrentPosition(showPosition)
