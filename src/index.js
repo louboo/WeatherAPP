e39be4a11282bdc5c778dcb642c2062227e1aafa
louboo <louisestephjones@gmail.com>
2021-12-04 19:18:47 +0000

    Added wind speed and humidity

--- src/index.js
+++ src/index.js
@@ -68,9 +68,14 @@ function showTemp(response) {
   temp.innerHTML = `${Math.round(response.data.main.temp)}°C`
   let weatherDesc = document.querySelector('h6')
   weatherDesc.innerHTML = `The weather today is...${response.data.weather[0].description}`
+  let humidity = response.data.main.humidity
+  let humidityDisplay = document.querySelector('humidity')
+  humidityDisplay.innerHTML = `Humidity ${humidity}`
+  let celsiusTemp = null
+  let windSpeedData = response.data.wind.wind.speed
+  let windDisplay = document.querySelector('#windSpeed')
+  windDisplay.innerHTML = `Wind speed: ${windSpeedData}`
 }
-
-let celsiusTemp = null
 
 function citySearch(city) {
   let apiKey = '270ea88a43eb3b55ea7828fd11903447'
