d7e523f5f1131d89b9dd4a39ac959139dd5dc557
louboo <louisestephjones@gmail.com>
2021-12-06 17:30:24 +0000

    forecast javascript string

--- src/index.js
+++ src/index.js
@@ -59,6 +59,41 @@ h4.innerHTML = `${hours}:${minutes}`
 
 // WEATHER API
 
+function displayForescast(response) {
+  let forecast = response.data.daily
+  let forecastElement = document.querySelector('#forecast')
+  let forecastHTML = `<div class="row">`
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max,
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min,
+          )}° </span>
+        </div>
+      </div>
+  `
+    }
+  })
+
+  forecastHTML = forecastHTML + `</div>`
+  forecastElement.innerHTML = forecastHTML
+}
+
 function showTemp(response) {
   let cityName = response.data.name
   let h1 = document.querySelector('h1')
