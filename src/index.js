f59d5e492105f98dcdcb9496bbe3e3c0923f7447
louboo <louisestephjones@gmail.com>
2021-12-07 15:27:45 +0000

    API integration - set up getForecast function and call apiUrl

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ h4.innerHTML = `${hours}:${minutes}`
 
 // WEATHER API
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector('#forecast') // selecting element
   // row
   let days = ['Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
@@ -88,6 +88,10 @@ function displayForecast() {
 
 function getForecast(coordinates) {
   console.log(coordinates)
+  let apiKey = '270ea88a43eb3b55ea7828fd11903447'
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`
+
+  axios.get(apiUrl).then(displayForecast)
 }
 
 function showTemp(response) {
@@ -165,4 +169,3 @@ let celsiusLink = document.querySelector('#celsius')
 celsiusLink.addEventListener('click', showCelsiusTemp)
 
 citySearch('Canterbury')
-displayForecast()
