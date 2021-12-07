09641cd2aac08d16ddb7f9867d135e44572563f4
louboo <louisestephjones@gmail.com>
2021-12-07 19:25:26 +0000

    Added Math.round

--- src/index.js
+++ src/index.js
@@ -73,13 +73,19 @@ function displayForecast(response) {
               <div class="col-2">
                 <div class="weather-forecast-date">${forecastDay.dt}</div>
                 <img
-                  src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
                   alt=""
                   width="42"
                 />
                 <div class="weather-forecast-temperatures">
-                  <span class="weather-forecast-temperature-max">${forecastDay.temp.max}° </span>
-                  <span class="weather-forecast-temperature-min">${forecastDay.temp.min}° </span>
+                  <span class="weather-forecast-temperature-max">${Math.round(
+                    forecastDay.temp.max,
+                  )}° </span>
+                  <span class="weather-forecast-temperature-min">${Math.round(
+                    forecastDay.temp.min,
+                  )}° </span>
                 </div>
               </div>
             `
